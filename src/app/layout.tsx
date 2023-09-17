40cb2d83d31c7dbfda17edad9ae4950854877278
mannaraees <79034666+mannaraees@users.noreply.github.com>
2023-09-17 05:33:50 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en" className=" scroll-smooth">
       <head>
         <meta charSet="utf-8" />
-        <meta content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
