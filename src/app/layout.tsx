d650515f8bca84ced16e341b703bda92252f0b62
mannaraees <79034666+mannaraees@users.noreply.github.com>
2023-09-17 05:05:22 +0400

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className=" scroll-smooth">
+      <head>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </head>
       <body className={` text-slate-100 scroll-smooth ${raleway.className}`}>
         {children}
       </body>
